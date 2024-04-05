083bcac783f5bc6d55d2b73004f73c5e7b0cbe18
Tag <sea15510@gmail.com>
2024-04-05 17:05:10 +0900

    [FIRST] 신규 프로젝트 추가
    BobPT(밥피티) 신규 프로젝트 추가
    
    -

---
+++ BobPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BobPT
+//
+//  Created by 윤해수 on 4/5/24.
+//
+
+import UIKit
+import NMapsMap
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
+    }
+}
