8d8d889bcd3fbd6b064aba973dae4f359aac5b07
강희창 <khc1113@ganghuichang-ui-MacBookPro.local>
2024-04-08 17:28:17 +0900

    Place

--- BobPT/ViewController/ResultViewController.swift
+++ BobPT/ViewController/ResultViewController.swift
@@ -15,26 +15,27 @@ class ResultViewController: UIViewController {
     
     var restaurant = [Restaurant]()
     var food: [String]?
-    
-    var keyword = "강남 중국"
-    
+//    var keyword = [place+food]
+    var place: String?
     let idKey = "6Omg7wmoaLIDTN99C0Ff"
     let secretKey = "R9vTsglyOb"
+    var save: [Root] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        naverSearch(keyword: "강남 중국")
-        
-        print(food)
+        guard let food, let place else { return }
+        for i in 0..<food.count {
+            let j = "\(place) \(food[i])"
+            naverSearch(keyword: j)
+            print(save)
+        }
+       
         // Do any additional setup after loading the view.
     }
     func naverSearch(keyword:String) {
         let endPoint = "https://openapi.naver.com/v1/search/local.json?query=\(keyword)"
         let params: Parameters = ["keyword": keyword]
         let headers: HTTPHeaders = ["X-Naver-Client-Id" : idKey, "X-Naver-Client-Secret" : secretKey]
-       
-      
-        
         let alamo = AF.request(
             endPoint,
             method: .get,
@@ -42,21 +43,26 @@ class ResultViewController: UIViewController {
             headers: headers
             )
         alamo.responseDecodable(of: Root.self) { response in
+            print(response)
             switch response.result {
-            case .success( let root): self.restaurant = root.items
-                DispatchQueue.main.async {
-                    self.todayLbl.text = "오늘의 추천 식당은"
-                    let message = root.items[0].title
-                    self.restLbl.text = message                    //음식점 랜덤 출력될 Label
-                    self.endLbl.text = "맛있게 드세요"
-                }
+            case .success(let root):
+                self.save.append(root)
+//                self.restaurant = root.items
+//                
+//                let randomPlace = keyword.randomElement()
+//                DispatchQueue.main.async {
+//                    self.todayLbl.text = "오늘의 추천 식당은"
+//                    let message = root.items[0].title
+//                    self.restLbl.text = message                    //음식점 랜덤 출력될 Label
+//                    self.endLbl.text = "맛있게 드세요"
+//                }
 
             case .failure(let error):
                 print(error.localizedDescription)
             }
-            let randomPlace = keyword.randomElement()   //랜덤출력?
+//            let randomPlace = keyword.randomElement()   //랜덤출력?
         }
-        }
+    }
             
     /*
     // MARK: - Navigation
@@ -68,6 +74,7 @@ class ResultViewController: UIViewController {
     }
     */
     @IBAction func mapBtn(_ sender: Any) {
+        let mapPlace = restaurant as? MapViewController
         performSegue(withIdentifier: "map", sender: nil)
     }   //식당위치확인 버튼 클릭 후 진웅님 맵뷰로 화면 전환
     
